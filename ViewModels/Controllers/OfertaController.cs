6d145a5282021743abbfff4afd1e989e892a650c
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-03-17 18:30:16 -0300

    Creacion de Oferta ViewModel

---
+++ ViewModels/Controllers/OfertaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewModels.Controllers
+{
+    public class OfertaController : Controller
+    {
+        // GET: Oferta
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewModels/ViewModels/OfertaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ViewModels.Models;
+
+namespace ViewModels.ViewModels
+{
+    public class OfertaViewModel
+    {
+        public Oferta Oferta { get; set; } //datos de la oferta
+        public List<Producto> Productos { get; set; }
+
+        public List<Proveedor> Proveedores { get; set; } 
+
+    }
+}
